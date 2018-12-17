196deb97a5b3644bf45ee2ca5eeea43d68b00283
kitte anderson <kitteanderson@gmail.com>
2018-12-17 02:50:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,15 +1,12 @@
-require 'pry'
+
 
 def hello_t(array)
   if block_given?
-    binding.pry
     i = 0
-binding.pry
+
     while i < array.length
       yield(array[i])
-      binding.pry
       i = i + 1
-      binding.pry
     end
 
     array
